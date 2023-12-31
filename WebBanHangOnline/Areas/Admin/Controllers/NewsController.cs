e08974e2e63d716dca7253254ab358841c5b461e
HP <HP@CUCCUNG>
2023-12-31 13:47:25 +0700

    Add project files.

---
+++ WebBanHangOnline/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+using PagedList;
+using System.Data.Entity.Infrastructure;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+   // [Authorize(Roles = "Admin,Employee")]
+    public class NewsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/News
+        public ActionResult Index(int? page, string Searchtext)
+        {
+            var pageSize = 10;
+            if (page == null)
+            {
+                page = 1;
+            }
+            IEnumerable<News> items = db.News.OrderByDescending(x => x.Id);
+            if (!string.IsNullOrEmpty(Searchtext))
+            {
+                items = items.Where(x => x.Alias.Contains(Searchtext) || x.Title.Contains(Searchtext));
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
+            return View(items);
+            
+        }
+
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(News model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    model.CreatedDate = DateTime.Now;
+                    model.CategoryId = 3;
+                    model.ModifiledDate = DateTime.Now;
+                    model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+
+                    db.News.Add(model);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                   
+                    ModelState.AddModelError("", "Lỗi xảy ra khi thêm tin tức. Vui lòng thử lại.");
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            var item = db.News.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(News model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.ModifiledDate = DateTime.UtcNow; // Thay vì DateTime.Now
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.News.Attach(model);
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    // Xử lý ngoại lệ nếu có
+                    ModelState.AddModelError("", $"Lỗi xảy ra khi cập nhật tin tức: {ex.Message}");
+                    return View(model);
+                }
+
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.News.Find(id);
+            if (item != null)
+            {
+                db.News.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult IsActive(int id)
+        {
+            var item = db.News.Find(id);
+            if (item != null)
+            {
+                item.IsActive = !item.IsActive;
+                db.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return Json(new { success = true, isAcive = item.IsActive });
+            }
+
+            return Json(new { success = false });
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAll(string ids)
+        {
+            if (!string.IsNullOrEmpty(ids))
+            {
+                var items = ids.Split(',');
+                if (items != null && items.Any())
+                {
+                    foreach (var item in items)
+                    {
+                        var obj = db.News.Find(Convert.ToInt32(item));
+                        if (obj != null)
+                        {
+                            db.News.Remove(obj);
+                        }
+                    }
+                    db.SaveChanges();
+                    return Json(new { success = true });
+                }
+            }
+            return Json(new { success = false });
+        }
+
+
+    }
+
+}
---
+++ WebBanHangOnline/Models/CommonAsbtract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models
+{
+    public class CommonAsbtract
+    {
+        public string Category { get; set; }    
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiledDate { get; set; }
+
+        public string Modifiledby { get; set; }
+    }
+}
